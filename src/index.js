25c829b8cf20d341f5cd3375095c26956a13b5de
Kartendsy <107519791+Kartendsy@users.noreply.github.com>
2025-10-24 19:37:44 +0000

    D

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import {createClient} from "https://esm.sh/@supabase/supabase-js@2";
 
+import kaplay from 'https://cdn.jsdelivr.net/npm/kaplay@3001.0.19/+esm'
+
 const supabase = createClient("https://zcuxsqfqmxzjnigimmya.supabase.co","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpjdXhzcWZxbXh6am5pZ2ltbXlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEzMjAxNzAsImV4cCI6MjA3Njg5NjE3MH0.4To2aWcUeXsQsMqAvBOPc_feC8nqNAk3zm9HKSQbX_4")
 
 async function start() {
-    const { data, error } = await supabase.from("users").select("*");
-    console.log(data);
-    
+    const K = kaplay({
+        canvas:document.getElementById("game-window")
+    })
 }
 
 
