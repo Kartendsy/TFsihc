6a3f5d09e9d09d8d3a8370b7e66a99835a6c5a65
Kartendsy <107519791+Kartendsy@users.noreply.github.com>
2025-10-24 18:57:56 +0000

    d

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ async function start() {
             console.log(resp);
         });
 
-    fetch("https://dogapi.dog/api/v2/breeds")
+    await fetch("https://dogapi.dog/api/v2/breeds")
         .then(resp => resp)
         .then(data => console.log(data))
 }
