2c3574214bb733ff502fcab8337f175f5faedbf2
Kartendsy <107519791+Kartendsy@users.noreply.github.com>
2025-10-24 19:11:11 +0000

    D

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-import axios from "https://esm.sh/axios@1.7.7";
+import {createClient} from "https://esm.sh/supabase";
 
+const supabase = createClient("https://zcuxsqfqmxzjnigimmya.supabase.co","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpjdXhzcWZxbXh6am5pZ2ltbXlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEzMjAxNzAsImV4cCI6MjA3Njg5NjE3MH0.4To2aWcUeXsQsMqAvBOPc_feC8nqNAk3zm9HKSQbX_4")
 
 async function start() {
-    await axios.get("https://dogapi.dog/api/v2/breeds")
-        .then(resp => {
-            console.log(resp);
-        });
-
-    await fetch("https://dogapi.dog/api/v2/breeds")
-        .then(resp => resp)
-        .then(data => console.log(data))
+    const { data, error } = await supabase.from("users").select("*");
+    console.log(data);
+    
 }
 
 
