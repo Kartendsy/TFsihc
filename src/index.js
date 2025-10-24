31b9a5f9b00140f0882f483865f338ebfef260b7
Kartendsy <107519791+Kartendsy@users.noreply.github.com>
2025-10-24 19:58:20 +0000

    D

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import {createClient} from "https://esm.sh/@supabase/supabase-js@2";
-
 import kaplay from 'https://cdn.jsdelivr.net/npm/kaplay@3001.0.19/+esm'
 
+
+
 const supabase = createClient("https://zcuxsqfqmxzjnigimmya.supabase.co","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpjdXhzcWZxbXh6am5pZ2ltbXlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEzMjAxNzAsImV4cCI6MjA3Njg5NjE3MH0.4To2aWcUeXsQsMqAvBOPc_feC8nqNAk3zm9HKSQbX_4")
+const tg = window.Telegram.WebApp;
+
+
+async function setup_user() {
+    const user = tg.initDataUnsafe?.user;
+
+    console.log(user);
+    
+}
+
 
 async function start() {
     const K = kaplay({
