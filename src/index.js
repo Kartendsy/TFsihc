0efd7d7f5cdbbaf81d2d777534416fb721b38da4
Kartendsy <107519791+Kartendsy@users.noreply.github.com>
2025-10-24 20:06:10 +0000

    D

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import kaplay from 'https://cdn.jsdelivr.net/npm/kaplay@3001.0.19/+esm'
 
 
 const usernameTag = document.getElementById("usernameTag");
-const coinTag = document.getElementById("coinTag")
+const coinTag = document.getElementById("coinTag");
+const photoTag = document.getElementById("photoTag");
 
 const supabase = createClient("https://zcuxsqfqmxzjnigimmya.supabase.co","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpjdXhzcWZxbXh6am5pZ2ltbXlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEzMjAxNzAsImV4cCI6MjA3Njg5NjE3MH0.4To2aWcUeXsQsMqAvBOPc_feC8nqNAk3zm9HKSQbX_4")
 const tg = window.Telegram.WebApp;
@@ -13,6 +14,7 @@ const tg = window.Telegram.WebApp;
 async function setup_user() {
     const user = tg.initDataUnsafe?.user;
     usernameTag.textContent = user?.first_name;
+    photoTag.src = user?.photo_url;
 }
 
 
