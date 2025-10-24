d2907cf86230b26fa0811963e2e8d0027446d0b2
Kartendsy <107519791+Kartendsy@users.noreply.github.com>
2025-10-24 17:50:24 +0000

    Update

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import kaplay from 'https://cdn.jsdelivr.net/npm/kaplay@3001.0.19/+esm'
+import dotenv from 'https://cdn.jsdelivr.net/npm/dotenv@17.2.3/+esm'
+import {createClient} from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.76.1/+esm'
+
+
+const supabase = createClient(process.env.DATABASE_URI, process.env.DATABASE_KEY);
 
 const usernameTag = document.getElementById("username-tag");
 const photoUser = document.getElementById("photo-user");
+const env = process.env;
 
 const tg = window.Telegram.WebApp;
 
@@ -9,14 +15,30 @@ const user = tg.initDataUnsafe?.user;
 tg.ready();
 tg.expand();
 
-usernameTag.textContent = tg.initDataUnsafe?.user?.username || "Anonim!";
-photoUser.src = user?.photo_url;
+async function setup_data_user() {
+    usernameTag.textContent = user?.first_name;
+    photoUser.src = user?.photo_url;
 
-console.log(tg.initDataUnsafe?.user);
+    // Login 
+    const {data:exists} = await supabase
+        .from("users")
+        .select("*")
+        .eq("id", user?.id);
+    
+    if (exists.length === 0) {
+        await supabase.from("users").insert({id:user?.id, username:user?.first_name, coin:100, backpack:[]});
+    }
+}
+
+async function startGame() {
+    setup_data_user();
+
+    const K = kaplay({
+        width:0,
+        height:512,
+        canvas:document.querySelector("#game-window")
+    });
+}
 
 
-kaplay({
-    width:0,
-    height:512,
-    canvas:document.querySelector("#game-window")
-});
+startGame();
