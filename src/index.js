dc55fa7213f0386c7e00b089e73b62b2fc09a1ac
Kartendsy <107519791+Kartendsy@users.noreply.github.com>
2025-10-24 19:12:23 +0000

    D

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {createClient} from "https://esm.sh/supabase";
+import {createClient} from "https://esm.sh/@supabase/supabase-js@2";
 
 const supabase = createClient("https://zcuxsqfqmxzjnigimmya.supabase.co","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpjdXhzcWZxbXh6am5pZ2ltbXlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEzMjAxNzAsImV4cCI6MjA3Njg5NjE3MH0.4To2aWcUeXsQsMqAvBOPc_feC8nqNAk3zm9HKSQbX_4")
 
