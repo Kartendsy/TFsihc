333bca1f766a02494fdab899285c7be256eac591
Kartendsy <107519791+Kartendsy@users.noreply.github.com>
2025-10-24 17:18:39 +0000

    update

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import kaplay from 'https://cdn.jsdelivr.net/npm/kaplay@3001.0.19/+esm'
 
 const usernameTag = document.getElementById("username-tag");
+const photoUser = document.getElementById("photo-user");
 
 const tg = window.Telegram.WebApp;
+
+const user = tg.initDataUnsafe?.user;
 tg.ready();
 tg.expand();
 
 usernameTag.textContent = tg.initDataUnsafe?.user?.username || "Anonim!";
+photoUser.src = user?.photo_url;
 
 console.log(tg.initDataUnsafe?.user);
 
